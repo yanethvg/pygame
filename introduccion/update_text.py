420543761a45e0fc07809d27dfe8e222ce0eef85
Zoila <zoilavillatoro6694@outlook.com>
2020-06-08 10:19:15 -0600

    test

---
+++ introduccion/update_text.py
@@ -0,0 +1,30 @@
+import pygame
+import sys
+
+pygame.init()
+
+width = 600
+height = 600
+
+surface = pygame.display.set_mode( (width, height)) # surface
+pygame.display.set_caption('Updated Text')
+#los colores se dominan con RGB
+#a traves de la clase color
+red = pygame.Color(255,0,0) # 0- 255 rojo
+green = pygame.Color(0,255,0)
+blue = pygame.Color(0,0,255)
+white = pygame.Color(255,255,255)
+black = pygame.Color(0,0,0)
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+   
+    #pintando la pantalla
+    surface.fill(white)
+
+    pygame.display.update()
