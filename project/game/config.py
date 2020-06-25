74b3d5fcb41c9c9ee7b4f6510b61a177a5f0377e
Zoila <zoilavillatoro6694@outlook.com>
2020-06-25 17:06:58 -0600

    added platform in project

---
+++ project/game/config.py
@@ -0,0 +1,12 @@
+WIDTH = 800
+HEIGHT = 400
+
+TITLE = 'Facilito game!!'
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GREEN = (69, 184, 61)
+BLUE = (78, 137, 193)
+RED = (184, 61, 88)
+YELLOW = (255, 255, 0)
+GREEN_LIGHT = (61, 174, 87)
--- project/game/game.py
+++ project/game/game.py
@@ -1,13 +1,16 @@
 import pygame
 import sys
 
+from .config import *
+from .platform import Platform
+
 
 class Game:
     def __init__(self):
         pygame.init()
 
-        self.surface = pygame.display.set_mode((800, 400))
-        pygame.display.set_caption('Facilito game !!')
+        self.surface = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption(TITLE)
 
         self.running = True
 
@@ -15,7 +18,15 @@ class Game:
         self.new()
 
     def new(self):
+        self.generate_elements()
         self.run()
+
+    def generate_elements(self):
+        self.platform = Platform()
+
+        self.sprites = pygame.sprite.Group()
+
+        self.sprites.add(self.platform)
 
     def run(self):
         while self.running:
@@ -31,7 +42,9 @@ class Game:
                 sys.exit()
 
     def draw(self):
-        pass
+        self.surface.fill(BLACK)
+
+        self.sprites.draw(self.surface)
 
     def update(self):
         pygame.display.flip()
